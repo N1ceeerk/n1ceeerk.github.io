332ae821917524e68c7d1a5b183029fd2bae1d27
N1ceeerk <n1ceeerk@gmail.com>
2020-09-18 14:44:59 +0300

    add

---
+++ lesson_24/js/main.js
@@ -0,0 +1,33 @@
+$(".login__header").click(function(){
+   $(".register").hide();
+   $(".login").show();
+   $(".login__header").removeClass("passive");
+   $(".register__header").removeClass("active");
+   $(".login__header").addClass('active');
+   $(".register__header").addClass('passive');
+   $(".register__button").hide();
+   $(".login__button").show();
+});
+
+$(".register__header").click(function(){
+   $(".register").show();
+   $(".login").hide();
+   $(".login__header").removeClass("active");
+   $(".register__header").removeClass("passive");
+   $(".login__header").addClass('passive');
+   $(".register__header").addClass('active');
+   $(".register__button").show();
+   $(".login__button").hide();
+});
+
+$('.login__button').hover(function(){
+   $(this).addClass('animate__animated animate__flash');
+}, function(){
+ $(this).removeClass('animate__animated animate__flash')
+});	
+
+$('.register__button').hover(function(){
+   $(this).addClass('animate__animated animate__flash');
+}, function(){
+ $(this).removeClass('animate__animated animate__flash')
+});   
